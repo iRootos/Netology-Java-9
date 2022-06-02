78e095dd1c5a0daa76e46ad1f7de19a43f595301
Rootos <rootos@hotmail.com>
2022-06-02 17:54:22 +0300

    init project

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String surname; // Поле для фамилии
+    public String patronymic; // Поле для отчества
+    public String phone; // Поле для номера телефона
+    public String passport; // поле для пасспорта
+    public Boolean subscription; // Поле подписки
+    public FormDate birthday;
+}
